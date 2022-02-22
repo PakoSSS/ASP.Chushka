46be405283b73154206545dc8b5e3268437b5fde
Student <Student@DESKTOP-GM02EHL>
2022-02-23 01:30:27 +1400

    Add project files.

---
+++ ASP.Chushka/Data/ChuskaContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.Chushka.Data
+{
+    public class ChuskaContext : DbContext
+    {
+         public ChuskaContext(DbContextOptions<ChuskaContext> options) : base(options)
+        {
+
+        }
+        public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<Products> Products { get; set; }
+        public virtual DbSet<Order> Orders { get; set; }
+    }
+}
---
+++ ASP.Chushka/Data/Products.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.Chushka.Data
+{
+    public enum TypeType { Food, Domestic , Health , Cosmetic , Other}
+    public class Products
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public TypeType Type { get; set; }
+        public virtual ICollection<Order> Orders { get; set; }
+    }
+}
